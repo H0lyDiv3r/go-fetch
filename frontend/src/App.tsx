56c4a227c7ff7a40281d6652249c7fe1ee5f9901
yuri <yohannesh.2018@gmail.com>
2025-01-22 10:23:57 +0300

    request state-manager with zustand! dark-mode only

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,19 @@ import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { useState } from "react";
 import { MakeRequest } from "../wailsjs/go/request/Request";
-import { useCountStore } from "./store/store";
+import { useRequestStore } from "./store/store";
 
 function App() {
-    const [url, setUrl] = useState("");
-    const [token, setToken] = useState("");
+    const {
+        url,
+        method,
+        params,
+        headers,
+        body,
+        setUrl,
+        setMethod,
+        setJsonBody,
+    } = useRequestStore();
     const tkn =
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VybmFtZSI6ImpvcyIsIkVtYWlsIjoiam9zcEBnbWFpbC5jb20ifQ.t3tK7UVG33YdWpudJUAFbBvoRG1qIJpMIiLr2h8BdHQ";
     const handleRequest = (url: string) => {
@@ -23,24 +31,15 @@ function App() {
             }),
         ).then((res) => console.log(res));
     };
-    const increment = useCountStore((state: any) => state.increment);
-    const count = useCountStore((state: any) => state.count);
-    const reset = useCountStore((state: any) => state.reset);
+
     return (
-        <div>
-            <Input
-                className="text-black"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-            />
-            <Input
+        <div className="dark">
+            <Input value={url} onChange={(e) => setUrl(e.target.value)} />
+            {/* <Input
                 className="text-black"
                 value={token}
                 onChange={(e) => setToken(e.target.value)}
-            />
-            <p style={{ color: "black" }}>{count}</p>
-            <Button onClick={increment}>inc</Button>
-            <Button onClick={reset}>inc</Button>
+            /> */}
             <Button onClick={() => handleRequest(url)}>request</Button>
         </div>
     );
--- frontend/src/store/store.tsx
+++ frontend/src/store/store.tsx
@@ -1,15 +1,72 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-type countStore = {
-    count: number;
-    increment: () => void;
-    decrement: () => void;
-    reset: () => void;
+enum Methods {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE",
+}
+type Headers = { [key: string]: string };
+type Params = { [key: string]: string };
+type JsonBody = { [key: string]: string | number | boolean | JsonBody };
+type requestStore = {
+    method: Methods;
+    url: string;
+    params: Params;
+    headers: Headers;
+    body: JsonBody;
+    setMethod: (method: Methods) => void;
+    setUrl: (url: string) => void;
+    addParam: (name: string, value: string) => void;
+    addHeader: (name: string, value: string) => void;
+    setJsonBody: (body: string) => void;
 };
 
-export const useCountStore = create<countStore>((set, get) => ({
-    count: 0,
-    increment: () => set((state) => ({ count: state.count + 1 })),
-    decrement: () => set((state) => ({ count: state.count - 1 })),
-    reset: () => set((state) => ({ count: state.count - get().count })),
-}));
+export const useRequestStore = create<requestStore>()(
+    persist(
+        (set, get) => ({
+            method: Methods.GET,
+            url: "",
+            params: {},
+            headers: {},
+            body: {},
+            setMethod: (method: Methods) =>
+                set((state) => ({ ...state, method: method })),
+            setUrl: (url: string) => set((state) => ({ ...state, url: url })),
+            addParam: (name: string, value: string) =>
+                set((state) => {
+                    const params = { ...state.params };
+                    params[name] = value;
+                    return { ...state, params: params };
+                }),
+            addHeader: (name: string, value: string) =>
+                set((state) => {
+                    const headers = { ...state.headers };
+                    headers[name] = value;
+                    return { ...state, headers: headers };
+                }),
+            setJsonBody: (body: string) =>
+                set((state) => {
+                    if (isValidJSON(body)) {
+                        const jsonBody = JSON.parse(body);
+                        return { ...state, body: JSON.parse(body) };
+                    }
+                    return { ...state, body: {} };
+                }),
+        }),
+        {
+            name: "requestStore",
+            storage: createJSONStorage(() => sessionStorage),
+        },
+    ),
+);
+
+const isValidJSON = (str: string) => {
+    try {
+        JSON.parse(str);
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
