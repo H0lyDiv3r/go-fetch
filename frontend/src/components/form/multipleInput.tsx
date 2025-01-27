b4e8d2c9394fabf8fcb488ce705ee46c3eaef6f6
yuri <yohannesh.2018@gmail.com>
2025-01-27 22:05:56 +0300

    multiple input half way

--- frontend/src/components/form/multipleInput.tsx
+++ frontend/src/components/form/multipleInput.tsx
@@ -1,21 +1,61 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FormInput } from "./formInput";
 import { Flex } from "@chakra-ui/react";
+import { DefaultButton } from "./buttons";
+import { useRequestStore } from "../../store/store";
 
 export const MultipleKeyValueInput = () => {
-  const [params, setParam] = useState([{ p1: "aa" }]);
+  const { addParam, params, setParamKey } = useRequestStore();
+  const [param, setParam] = useState([
+    { id: 1, key: "Auth", value: "TOKEN GOES HERE" },
+  ]);
+
+  const handleAddField = () => {
+    setParam((params) => {
+      return [
+        ...params,
+        { id: params[params.length - 1].id + 1, key: "", value: "" },
+      ];
+    });
+  };
+
+  // const setParamKey = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+  //   const newKey = e.target.value;
+
+  //   setParam((prevParams) => {
+  //     return prevParams.map((param) =>
+  //       param.id === id ? { ...param, key: newKey } : param,
+  //     );
+  //   });
+  // };
+
+  const setParamVal = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    setParam((prev) => {
+      return prev.map((param) =>
+        param.id === id ? { ...param, value: e.target.value } : param,
+      );
+    });
+  };
+
+  useEffect(() => {
+    console.log(params);
+  }, []);
+
   return (
     <>
       {params.map((item: any) => (
-        <Flex>
-          {Object.keys(item).map((param, idx) => (
-            <>
-              <FormInput value={param} setValue={() => console.log()} />
-              <FormInput value={item[param]} setValue={() => console.log()} />
-            </>
-          ))}
+        <Flex key={item.id} my={2}>
+          <FormInput
+            value={item.key}
+            setValue={(e) => setParamKey(e, item.id)}
+          />
+          <FormInput
+            value={item.value}
+            setValue={(e) => setParamVal(e, item.id)}
+          />
         </Flex>
       ))}
+      <button onClick={addParam}>add</button>
     </>
   );
 };
--- frontend/src/store/store.tsx
+++ frontend/src/store/store.tsx
@@ -1,72 +1,109 @@
+import React from "react";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
+// const setParamKey = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+//   const newKey = e.target.value;
+
+//   setParam((prevParams) => {
+//     return prevParams.map((param) =>
+//       param.id === id ? { ...param, key: newKey } : param,
+//     );
+//   });
+// };
+
+// const setParamVal = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+//   setParam((prev) => {
+//     return prev.map((param) =>
+//       param.id === id ? { ...param, value: e.target.value } : param,
+//     );
+//   });
+// };
 export enum Methods {
-    GET = "GET",
-    POST = "POST",
-    PUT = "PUT",
-    DELETE = "DELETE",
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
 }
 type Headers = { [key: string]: string };
-type Params = { [key: string]: string };
+type Params = { id: number; key: string; value: string }[];
 type JsonBody = { [key: string]: string | number | boolean | JsonBody };
 type requestStore = {
-    method: Methods;
-    url: string;
-    params: Params;
-    headers: Headers;
-    body: JsonBody;
-    setMethod: (method: Methods) => void;
-    setUrl: (url: string) => void;
-    addParam: (name: string, value: string) => void;
-    addHeader: (name: string, value: string) => void;
-    setJsonBody: (body: string) => void;
+  method: Methods;
+  url: string;
+  params: Params;
+  headers: Headers;
+  body: JsonBody;
+  setMethod: (method: Methods) => void;
+  setUrl: (url: string) => void;
+  addParam: () => void;
+  setParamKey: (e: React.ChangeEvent<HTMLInputElement>, id: number) => void;
+  // setParamValue: (e: React.ChangeEvent<HTMLInputElement>, id: number) => void;
+  addHeader: (name: string, value: string) => void;
+  setJsonBody: (body: string) => void;
 };
 
 export const useRequestStore = create<requestStore>()(
-    persist(
-        (set, get) => ({
-            method: Methods.GET,
-            url: "",
-            params: {},
-            headers: {},
-            body: {},
-            setMethod: (method: Methods) =>
-                set((state) => ({ ...state, method: method })),
-            setUrl: (url: string) => set((state) => ({ ...state, url: url })),
-            addParam: (name: string, value: string) =>
-                set((state) => {
-                    const params = { ...state.params };
-                    params[name] = value;
-                    return { ...state, params: params };
-                }),
-            addHeader: (name: string, value: string) =>
-                set((state) => {
-                    const headers = { ...state.headers };
-                    headers[name] = value;
-                    return { ...state, headers: headers };
-                }),
-            setJsonBody: (body: string) =>
-                set((state) => {
-                    if (isValidJSON(body)) {
-                        const jsonBody = JSON.parse(body);
-                        return { ...state, body: JSON.parse(body) };
-                    }
-                    return { ...state, body: {} };
-                }),
+  persist(
+    (set, get) => ({
+      method: Methods.GET,
+      url: "",
+      params: [{ id: 1, key: "Auth", value: "TOKEN GOES HERE" }],
+      headers: {},
+      body: {},
+      setMethod: (method: Methods) =>
+        set((state) => ({ ...state, method: method })),
+      setUrl: (url: string) => set((state) => ({ ...state, url: url })),
+      addParam: () =>
+        set((state) => {
+          const params = [
+            ...state.params,
+            {
+              id: state.params[state.params.length - 1].id + 1,
+              key: "",
+              value: "",
+            },
+          ];
+          return { ...state, params: params };
         }),
-        {
-            name: "requestStore",
-            storage: createJSONStorage(() => sessionStorage),
-        },
-    ),
+      setParamKey: (e: React.ChangeEvent<HTMLInputElement>, id: number) =>
+        set((state) => {
+          const newKey = e.target.value;
+
+          return {
+            ...state,
+            params: state.params.map((param) => {
+              return param.id === id ? { ...param, key: newKey } : param;
+            }),
+          };
+        }),
+      addHeader: (name: string, value: string) =>
+        set((state) => {
+          const headers = { ...state.headers };
+          headers[name] = value;
+          return { ...state, headers: headers };
+        }),
+      setJsonBody: (body: string) =>
+        set((state) => {
+          if (isValidJSON(body)) {
+            const jsonBody = JSON.parse(body);
+            return { ...state, body: JSON.parse(body) };
+          }
+          return { ...state, body: {} };
+        }),
+    }),
+    {
+      name: "requestStore",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
 );
 
 const isValidJSON = (str: string) => {
-    try {
-        JSON.parse(str);
-        return true;
-    } catch (e) {
-        return false;
-    }
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (e) {
+    return false;
+  }
 };
